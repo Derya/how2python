3eec3250f41467b08c2796c58f7b2302f659a905
Derya Aydede <derya.aydede@gmail.com>
2019-02-21 22:09:36 -0800

    initial commit

---
+++ repl/example_1.py
@@ -0,0 +1,18 @@
+# describe available commands
+print("== COMMANDS ==")
+print("hello -> say hello")
+print("sing -> sing a song")
+
+# read the user's command
+user_command = input('Enter your command:\n')
+
+if user_command == "hello":
+  # say hello
+  print("Hello!")
+elif user_command == "sing":
+  # sing a song
+  print("Yo listen up, here's the story / About a little guy that / lives in a blue world / And all day and all night "
+        "and everything he sees is just blue...")
+else:
+  # command is unknown, do nothing other than alerting the user as such
+  print(f"Unknown command '{user_command}'!")
